e7a6f04d6781bb7f891ac15c4e43f9cca21a9429
Yuxuan Zhang <yuxuan@yuxuanzhang.net>
2021-12-28 17:25:13 +0800

    Bug fix

--- main.js
+++ main.js
@@ -1,15 +1,3 @@
-import { Worker } from 'worker_threads';
-import { Motor } from './vehicle/Motor.js';
-const Connection = new Worker('./vehicle/connection.js');
-Connection.on('message', (params) => {
-    if ('hlt' in params) {
-        // Halt all motion
-        Motor.exec();
-    } else {
-        Motor.drive(params);
-    }
-});
-
 import { Service } from './util/Service.js';
 Service.init(
     'vehicle/Connection',
